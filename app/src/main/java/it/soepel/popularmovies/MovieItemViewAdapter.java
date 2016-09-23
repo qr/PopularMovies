6f988627ff336a082891040b21e2c419bebcabe5
Bart Elferink <bart@soepel.it>
2016-09-23 15:16:28 +0200

    removed unused annotations

--- app/src/main/java/it/soepel/popularmovies/MovieItemViewAdapter.java
+++ app/src/main/java/it/soepel/popularmovies/MovieItemViewAdapter.java
@@ -2,14 +2,13 @@ package it.soepel.popularmovies;
 
 import android.content.Context;
 import android.net.Uri;
-import android.support.v7.widget.AppCompatImageView;
+
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
